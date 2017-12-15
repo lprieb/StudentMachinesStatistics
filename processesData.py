06db1caff97e01a33f9e5565bf19395a9443c2b4
Luis Prieb <lprieb@nd.edu>
2017-12-15 00:28:28 -0500

    Update all files

--- processesData.py
+++ processesData.py
@@ -1,27 +1,30 @@
 import math
 class ProcessData:
-	def __init__(self,user,pid,cpu_u,mem_u,vsz,rss,tty,stat,start,running_time,command):
-		self.user = user
-		self.pid = pid
-		self.avg_cpu_u = float(cpu_u)
-		self.avg_mem_u = float(mem_u)
-		self.avg_vsz = float(vsz)
-		self.avg_rss = float(rss)
-		self.max_cpu_u = float(cpu_u)
-		self.max_mem_u = float(mem_u)
-		self.max_vsz = float(vsz)
-		self.max_rss = float(rss)
-		self.var_cpu_u = 0.0
-		self.var_mem_u = 0.0
-		self.var_vsz = 0.0
-		self.var_rss = 0.0
-		self.tty = tty
-		self.stat = stat
-		self.start = start
-		self.running_time = running_time
-		self.command = command
-		self.end_time = 0
-		self.measures = 1
+	def __init__(self,netid=None,pid=None,cpu_u=None,mem_u=None,vsz=None,rss=None,tty=None,stat=None,start=None,machineid=None,command=None,tdict=None):
+		if(tdict):
+		    self.updateFromDict(tdict)
+		else:
+		    self.netid = netid
+		    self.pid = pid
+		    self.avg_cpu_u = float(cpu_u)
+		    self.avg_mem_u = float(mem_u)
+		    self.avg_vsz = float(vsz)
+		    self.avg_rss = float(rss)
+		    self.max_cpu_u = float(cpu_u)
+		    self.max_mem_u = float(mem_u)
+		    self.max_vsz = float(vsz)
+		    self.max_rss = float(rss)
+		    self.var_cpu_u = 0.0
+		    self.var_mem_u = 0.0
+		    self.var_vsz = 0.0
+		    self.var_rss = 0.0
+		    self.tty = tty
+		    self.stat = stat
+		    self.start = start
+		    self.command = command
+		    self.end_time = 0
+		    self.machineid = machineid
+		    self.measures = 1
 	def update_data(self,cpu_u,mem_u,vsz,rss):
 		cpu_u = float(cpu_u)
 		mem_u = float(mem_u)
@@ -50,3 +53,26 @@ class ProcessData:
 		self.var_vsz = (1.0/self.measures)*(self.var_vsz*(self.measures-1)+math.pow(old_avg_vsz,2)*self.measures+math.pow(vsz,2) - math.pow(self.avg_vsz,2)*(self.measures+1))
 		self.var_rss = (1.0/self.measures)*(self.var_rss*(self.measures-1)+math.pow(old_avg_rss,2)*self.measures+math.pow(rss,2) - math.pow(self.avg_rss,2)*(self.measures+1))
 
+	def updateFromDict(self,tdict):
+	    self.netid = tdict['netid']
+	    self.pid = tdict['pid']
+	    self.avg_cpu_u = tdict['average_cpu_usage']
+	    self.avg_mem_u = tdict['average_mem_usage']
+	    self.avg_vsz = tdict['average_vsz']
+	    self.avg_rss = tdict['average_rss']
+	    self.max_cpu_u = tdict['max_cpu_usage']
+	    self.max_mem_u = tdict['max_mem_usage']
+	    self.max_vsz = tdict['max_vsz']
+	    self.max_rss = tdict['max_rss']
+	    self.var_cpu_u = tdict['variance_cpu_usage']
+	    self.var_mem_u = tdict['variance_mem_usage']
+	    self.var_vsz = tdict['variance_vsz']
+	    self.var_rss = tdict['variance_rss']
+	    self.tty = tdict['tty']
+	    self.stat = tdict['stat']
+	    self.start = tdict['startTime']
+	    self.command = tdict['command']
+	    self.end_time = tdict['endTime']
+	    self.measures = tdict['measures']
+	    self.machineid = tdict['machineid']
+	    
--- scan_data.py
+++ scan_data.py
@@ -7,7 +7,86 @@ import math
 import traceback
 from usersData import *
 from processesData import *
+import sqlalchemy as sql
+import datetime
+import sys
+#import pymysql
+#from pymysql.err import *
+################################## GLOBALS ######################################
 
+headers = ["USER","PID","%CPU","%MEM","VSZ","RSS","TTY","STAT","START","RUNNING_TIME","COMMAND"]
+
+last_write = time.time()
+last_sample = time.time()
+WRITE_INTERVAL = 20 # Seconds
+WRITE_COUNT = 0 # Number of writes
+BACKUP_WRITE_COUNT = 100 # STORE A BACKUP EVERY 100 Writes
+SAMPLE_INTERVAL = 3 # Seconds
+previous_pids = set()
+INVALID_NETIDS = set()
+insert_proc_on_next = set()
+update_proc_on_next = set()
+insert_user_on_next = set()
+faculty_dict = dict()
+staff_dict = dict()
+student_dict = dict()
+process_dict = dict()
+MACHINE_ID = None
+firstTime = True # Used first time updating databases to prevent errors
+found_pids = set()
+ended_pids_global = set()
+filter_pids = set()
+if(len(sys.argv) != 2):
+    print "Incorrect number of arguments. Call:\n{} machineid".format(sys.argv[0])
+    sys.exit(1)
+else:
+    MACHINE_ID = sys.argv[1]
+
+################################ DB INTIALIZATION ####################################
+dbs = "mysql+pymysql://lprieb:open1234@dsg1.crc.nd.edu/feb31"
+engine = sql.create_engine(dbs, echo=False)
+conn = engine.connect()
+metadata = sql.MetaData()
+user = sql.Table('user', metadata,
+    sql.Column('netid',sql.String(20),primary_key=True),
+    sql.Column('firstName',sql.String(30),nullable=False),
+    sql.Column('lastName',sql.String(30),nullable=False),
+    sql.Column('affiliation',sql.String(50)),
+    sql.Column('college', sql.String(50)),
+    sql.Column('classLevel',sql.String(15)),
+    )
+machine = sql.Table('machine',metadata,
+    sql.Column('machineid',sql.String(20),primary_key=True),
+    sql.Column('total_memory',sql.BigInteger),
+    sql.Column('num_cpus',sql.Integer),
+    sql.Column('total_swap',sql.BigInteger),
+    )
+
+process = sql.Table('process2',metadata,
+    sql.Column('netid',sql.String(20),sql.ForeignKey('user.netid'),primary_key=True),
+    sql.Column('pid',sql.Integer, primary_key=True),
+    sql.Column('machineid',sql.String(20),sql.ForeignKey('machine.machineid'),primary_key=True),
+    sql.Column('average_cpu_usage',sql.Float),
+    sql.Column('average_mem_usage',sql.Float),
+    sql.Column('average_rss',sql.Float),
+    sql.Column('average_vsz',sql.Float),
+    sql.Column('max_cpu_usage',sql.Float),
+    sql.Column('max_mem_usage',sql.Float),
+    sql.Column('max_vsz',sql.Float),
+    sql.Column('max_rss',sql.Float),
+    sql.Column('variance_cpu_usage',sql.Float),
+    sql.Column('variance_mem_usage',sql.Float),
+    sql.Column('variance_vsz',sql.Float),
+    sql.Column('variance_rss',sql.Float),
+    sql.Column('tty',sql.String(30)),
+    sql.Column('stat',sql.String(10)),
+    sql.Column('startTime',sql.DateTime,primary_key=True),
+    sql.Column('endTime',sql.DateTime,default=datetime.datetime(2000,1,1,0,0,0,0)),
+    sql.Column('command', sql.String(500)),
+    sql.Column('measures',sql.Integer)
+    )
+
+metadata.create_all(engine) # won't create if already exists
 ######################### FUNCTION DEFINITION ###################################
 # Write Data to file
 def writeData(processDict, studentDict, facultyDict, staffDict, write_count, backup_write_count):
@@ -29,6 +108,97 @@ def writeData(processDict, studentDict, facultyDict, staffDict, write_count, bac
 	cPickle.dump(facultyDict,open("data/faculty_data.p","wb"))
 	cPickle.dump(staffDict,open("data/staff_data.p","wb"))
 
+def writeToDatabase(processDict):
+	global firstTime
+	real_proc_to_update = update_proc_on_next - insert_proc_on_next
+	if(firstTime):
+		firstTime = False
+		to_remove = list()
+		for p in insert_proc_on_next:
+			if p in found_pids:
+				to_remove.append(p)
+		for p in to_remove:
+			insert_proc_on_next.remove(p)
+	for u in insert_user_on_next:
+		result = conn.execute(sql.select([user]).where(user.c.netid == u.netid))
+		if(result.rowcount == 0):
+			try:
+				ins = user.insert().values(netid=u.netid,affiliation=u.title,firstName=u.firstname,lastName=u.lastname,college=u.college,classLevel=u.grade)
+				result = conn.execute(ins)
+			except sql.exc.IntegrityError as e:
+				print "User Error: {}".format(e)
+				with open("log.error","a") as f:
+					f.write("[{}]: {}\n".format(datetime.datetime.now(),str(e)))
+					#insert_user_on_next.remove(u)
+		
+	for proc in insert_proc_on_next:
+		try:
+			if(proc in filter_pids): continue
+
+			procObj = processDict[proc]
+			ins = process.insert().values(
+				netid= procObj.netid,
+				pid = procObj.pid,
+				machineid = procObj.machineid,
+				average_cpu_usage =procObj.avg_cpu_u,
+				average_mem_usage = procObj.avg_mem_u,
+				average_rss = procObj.avg_rss,
+				average_vsz = procObj.avg_vsz,
+				max_cpu_usage = procObj.max_cpu_u,
+				max_mem_usage = procObj.max_mem_u,
+				max_vsz  = procObj.max_vsz,
+				max_rss = procObj.max_rss,
+				variance_cpu_usage = procObj.var_cpu_u,
+				variance_mem_usage  = procObj.var_mem_u,
+				variance_vsz  = procObj.var_vsz,
+				variance_rss = procObj.var_rss,
+				tty = procObj.tty,
+				stat = procObj.stat,
+				startTime = procObj.start,
+				endTime = procObj.end_time,
+				command = procObj.command,
+				measures = procObj.measures
+			)
+			result = conn.execute(ins)
+		except sql.exc.IntegrityError as e:
+			print "Insert Error: {}".format(e)
+			filter_pids.add(proc)
+			with open("insert_log.error","a") as f:
+				f.write("[{}]: {}\n".format(datetime.datetime.now(),str(e)))
+	for proc in real_proc_to_update:
+		try:
+			procObj = processDict[proc]
+			ins = process.update().where(sql.and_(
+					process.c.netid==procObj.netid,
+					process.c.startTime==procObj.start,
+					process.c.machineid==procObj.machineid,
+					process.c.pid== procObj.pid)).values(
+						average_cpu_usage =procObj.avg_cpu_u,
+						average_mem_usage = procObj.avg_mem_u,
+						average_rss = procObj.avg_rss,
+						average_vsz = procObj.avg_vsz,
+						max_cpu_usage = procObj.max_cpu_u,
+						max_mem_usage = procObj.max_mem_u,
+						max_vsz  = procObj.max_vsz,
+						max_rss = procObj.max_rss,
+						variance_cpu_usage = procObj.var_cpu_u,
+						variance_mem_usage  = procObj.var_mem_u,
+						variance_vsz  = procObj.var_vsz,
+						variance_rss = procObj.var_rss,
+						endTime = procObj.end_time,
+						measures = procObj.measures
+						)
+
+			result = conn.execute(ins)
+		except sql.exc.IntegrityError as e:
+		    print "Update Error: {}".format(e)
+		    with open("updat_log.error","a") as f:
+			    f.write("[{}]: {}\n".format(datetime.datetime.now(),str(e)))
+			#insert_proc_on_next.remove(proc)
+	insert_user_on_next.clear()
+	update_proc_on_next.clear()
+	insert_proc_on_next.clear()
+    
 def requestData(netid):
 	global INVALID_NETIDS
 	r = requests.get("http://ur.nd.edu/request/eds.php?uid={}&full_response=true".format(str(netid)))
@@ -36,30 +206,22 @@ def requestData(netid):
 		resp = json.loads(r.content.decode("utf-8"))
 		if resp['ndprimaryaffiliation']  == "Faculty": 
 			return "Faculty", FacultyData(netid, resp["ndprimaryaffiliation"], resp["givenname"], resp["sn"], resp["ndtoplevelprimarydepartment"])
-		elif resp['ndprimaryaffiliation'] == "Staff:":
+		elif resp['ndprimaryaffiliation'] == "Staff":
 			return "Staff", StaffData(netid, resp["ndprimaryaffiliation"], resp["givenname"], resp["sn"], resp["ndtoplevelprimarydepartment"])
 		elif resp['ndprimaryaffiliation'] == "Student":
 			return "Student", StudentData(netid, resp["ndprimaryaffiliation"], resp["givenname"], resp["sn"], resp["ndtoplevelprimarydepartment"], resp["ndlevel"])
-	except ValueError:
+		else:
+			print "Unexpect affiliation {}".format(resp['ndprimaryaffiliation'])
+			raise ValueError
+	except (ValueError,KeyError) as e:
 		INVALID_NETIDS.add(netid)
-		return "Error", None
+		return "Invali Netid Error", None
 
 
-################################## GLOBALS ######################################
-
-headers = ["USER","PID","%CPU","%MEM","VSZ","RSS","TTY","STAT","START","RUNNING_TIME","COMMAND"]
-
-last_write = time.time()
-last_sample = time.time()
-WRITE_INTERVAL = 20 # Seconds
-WRITE_COUNT = 0 # Number of writes
-BACKUP_WRITE_COUNT = 100 # STORE A BACKUP EVERY 100 Writes
-SAMPLE_INTERVAL = 3 # Seconds
-previous_pids = set()
-INVALID_NETIDS = set()
 
 ################################ DATA FILES ####################################
 # Processes data file
+'''
 if(os.path.isfile("./data/process_data.p")):
 	process_dict = cPickle.load(open("data/process_data.p","rb"))
 	for key,value in process_dict.items():
@@ -93,6 +255,23 @@ if (os.path.isfile("./data/staff_data.p")):
 else:
 	staff_dict = {}
 
+'''
+
+############################## UPDATE FROM DATABASE ############################
+result = conn.execute(sql.select([user]))
+for row in result:
+    if row["affiliation"] == "Student":
+	student_dict[row["netid"]] = StudentData(tdict=row)
+    elif row["affiliation"] == "Staff":
+	staff_dict[row["netid"]] = StaffData(tdict=row)
+    elif row["affiliation"] == "Faculty":
+	faculty_dict[row["netid"]] = FacultyData(tdict=row)
+
+result = conn.execute(sql.select([process]))
+for row in result:
+	process_dict[(row["pid"],row["startTime"])] = ProcessData(tdict=row)
+	found_pids.add((row["pid"],row["startTime"]))	
+
 
 ############################## INFINITE LOOP ################################### 
 
@@ -101,97 +280,146 @@ try:
 	print "Syncing up database. . .",
 	for pid_start, process_data in process_dict.items():
 		# New user
-		netid = process_data.user
+		netid = process_data.netid
 		if netid not in student_dict.keys() and netid not in faculty_dict.keys():
 			title, usersData = requestData(netid)
 			if title == "Student":
 				student_dict[netid] = usersData
+				insert_user_on_next.add(usersData)
 			elif title == "Faculty":
 				faculty_dict[netid] = usersData
+				insert_user_on_next.add(usersData)
 			elif title == "Staff":
 				staff_dict[netid] = usersData
+				insert_user_on_next.add(usersData)
 	print "DONE"
-			
 	while True:
-		current_pids = set()
-		# Obtain Data
-		output = subprocess.check_output(["ps","aux"])
-		session_dict = {} 
-			# key = (netid, pid)
-			# value = temp_dict
-		for i, line in enumerate(output.splitlines()):
-			if i == 0:
-				continue
-			elems = line.split()
-			temp_dict = dict()
-			#print elems
-			if(len(elems) > len(headers)):
-				elems = elems[0:10] + ["".join(elems[10:]),] # Somtimes commands has lines in between
-			for i in range(len(elems)):
-				temp_dict[headers[i]] = elems[i]
+	    try:
+			current_pids = set()
+			# Obtain Data
+			output = subprocess.check_output(["ps","aux"])
+			session_dict = {} 
+				# key = (startTime, pid)
+				# value = temp_dict
+			for i, line in enumerate(output.splitlines()):
+				if i == 0:
+					continue
+				elems = line.split()
+				temp_dict = dict()
+				#print elems
+				if(len(elems) > len(headers)):
+					elems = elems[0:10] + [" ".join(elems[10:]),] # Somtimes commands has lines in between
+				for i in range(len(elems)):
+					temp_dict[headers[i]] = elems[i]
 
-			# New user in current session
-			netid = temp_dict["USER"]
-			# Don't waste time making requests fo invalid netids
-			if netid in INVALID_NETIDS:
-				continue
-			if netid not in student_dict.keys() and netid not in faculty_dict.keys() and netid not in staff_dict.keys():
-				title, usersData = requestData(netid)
-				if title == "Student":
-					student_dict[netid] = usersData
-				elif title == "Faculty":
-					faculty_dict[netid] = usersData
-				elif title == "Staff": 
-					staff_dict[netid] = usersData
-				else: 
-					continue # skip root, ndsc, and other invalid netids
+				if(temp_dict["USER"] == "lprieb" and temp_dict["COMMAND"] == "ps aux"):
+					continue # Dont want to store our data collection process
+				# New user in current session
+				netid = temp_dict["USER"]
+				# Don't waste time making requests fo invalid netids
+				if netid in INVALID_NETIDS:
+					continue
+				if netid not in student_dict.keys() and netid not in faculty_dict.keys() and netid not in staff_dict.keys():
+					title, usersData = requestData(netid)
+					insert_users_on_next = usersData
+					if title == "Student":
+						student_dict[netid] = usersData
+						insert_user_on_next.add(usersData)
+					elif title == "Faculty":
+						faculty_dict[netid] = usersData
+						insert_user_on_next.add(usersData)
+					elif title == "Staff": 
+						staff_dict[netid] = usersData
+						insert_user_on_next.add(usersData)
+					else: 
+						continue # skip root, ndsc, and other invalid netids
 
-			#process_dict[(temp_dict["PID"],temp_dict["START"])]= temp_dict
-			#current_pids.add((temp_dict["PID"],temp_dict["START"]))
-			current_pids.add((temp_dict["USER"], temp_dict["PID"]))
-			session_dict[(temp_dict["USER"], temp_dict["PID"])] = temp_dict
-		
+				# Correct starttime
+				try:
+					temp_dict["START"] = datetime.datetime.strptime(temp_dict["START"],"%H:%M")
+					temp_dict["START"] = temp_dict["START"].replace(year=datetime.datetime.now().year)
+					temp_dict["START"] = temp_dict["START"].replace(month=datetime.datetime.now().month)
+				except Exception:
+					temp_dict["START"] = datetime.datetime.strptime(temp_dict["START"],"%b%d")
+				
+				# Correct pid
+				temp_dict["PID"] = int(temp_dict["PID"])
+				session_dict[(temp_dict["PID"],temp_dict["START"])]= temp_dict
+				current_pids.add((temp_dict["PID"],temp_dict["START"]))
+				#current_pids.add((temp_dict["USER"], temp_dict["PID"]))
+				#session_dict[(temp_dict["USER"], temp_dict["PID"])] = temp_dict
+			
 
-		
-		# Update data files
-		ended_pids = previous_pids - current_pids 
-		new_pids = current_pids - previous_pids
-		stayed_pids = current_pids & previous_pids # set intersection
-		previous_pids = current_pids
+			
+			# Update data files
+			ended_pids = previous_pids - current_pids 
+			new_pids = current_pids - previous_pids
+			stayed_pids = current_pids & previous_pids # set intersection
+			previous_pids = current_pids
 
-		for p in stayed_pids:
-			process = process_dict[p]
-			temp_dict = session_dict[p]
-			process.update_data(temp_dict["%CPU"],temp_dict["%MEM"],temp_dict["VSZ"],temp_dict["RSS"])
-			process_dict[p] = process
-		for p in new_pids:
-			temp_dict = session_dict[p]
-			new_proc = ProcessData(temp_dict["USER"],temp_dict["PID"],temp_dict["%CPU"],temp_dict["%MEM"],temp_dict["VSZ"],temp_dict["RSS"],temp_dict["TTY"],temp_dict["STAT"],temp_dict["START"],temp_dict["RUNNING_TIME"],temp_dict["COMMAND"])
-			process_dict[p] = new_proc
-		for p in ended_pids:
-			process = process_dict[p]
-			process.end_time = time.asctime(time.localtime())
-			process_dict[p] = process
-		
-		# Write data to file if interval met
-		if(time.time() - last_write > WRITE_INTERVAL):
-			t = threading.Thread(target=writeData,args=(process_dict, student_dict, faculty_dict, staff_dict, WRITE_COUNT,BACKUP_WRITE_COUNT))
-			t.start() # Write in separate Thread
-			last_write = time.time()
-		
-		if(time.time() - last_sample > SAMPLE_INTERVAL):
-			print("Time wasn't enough to finish sampling")
-			last_sample = time.time()
-		else:
-			time.sleep(SAMPLE_INTERVAL-(time.time()-last_sample))
-			last_sample = time.time()
+			for p in stayed_pids:
+				cprocess = process_dict[p]
+				temp_dict = session_dict[p]
+				
+				cprocess.update_data(temp_dict["%CPU"],temp_dict["%MEM"],temp_dict["VSZ"],temp_dict["RSS"])
+				process_dict[p] = cprocess
+				update_proc_on_next.add(p)
+			for p in new_pids:
+				#if(firstTime):
+				if not p in process_dict:
+					temp_dict = session_dict[p]
+					new_proc = ProcessData(temp_dict["USER"],temp_dict["PID"],temp_dict["%CPU"],temp_dict["%MEM"],temp_dict["VSZ"],temp_dict["RSS"],temp_dict["TTY"],temp_dict["STAT"],temp_dict["START"],MACHINE_ID,temp_dict["COMMAND"])
+					process_dict[p] = new_proc
+					insert_proc_on_next.add(p)
+				#else:
+				#	temp_dict = session_dict[p]
+				#	new_proc = ProcessData(temp_dict["USER"],temp_dict["PID"],temp_dict["%CPU"],temp_dict["%MEM"],temp_dict["VSZ"],temp_dict["RSS"],temp_dict["TTY"],temp_dict["STAT"],temp_dict["START"],MACHINE_ID,temp_dict["COMMAND"])
+				#	process_dict[p] = new_proc
+				#	insert_proc_on_next.add(p)
+					
+			for p in ended_pids:
+				cprocess = process_dict[p]
+				cprocess.end_time = datetime.datetime.now()
+				process_dict[p] = cprocess
+				update_proc_on_next.add(p)
+				ended_pids_global.add(p)
+			
+			# Write data to file if interval met
+			if(time.time() - last_write > WRITE_INTERVAL):
+				#t = threading.Thread(target=writeToDatabase,args=(process_dict, student_dict, faculty_dict, staff_dict))
+				#t.start() # Write in separate Thread
+				writeToDatabase(process_dict)
+				last_write = time.time()
+			
+			if(time.time() - last_sample > SAMPLE_INTERVAL):
+				print("Time wasn't enough to finish sampling")
+				last_sample = time.time()
+			else:
+				time.sleep(SAMPLE_INTERVAL-(time.time()-last_sample))
+				last_sample = time.time()
 
+	    except sql.exc.IntegrityError as e:
+		    #traceback.print_exc()
+		    print "Integrity Error: {}".format(e)
+		    with open("integrity_log.error","a") as f:
+			    f.write("[{}]: {}\n".format(datetime.datetime.now(),str(e)))
+	    except Exception as e:
+			traceback.print_exc()
+			print "Error: {}".format(e)
+			sys.exit(1)
+			with open("log.error","a") as f:
+				f.write("[{}]: {}\n".format(datetime.datetime.now(),str(e)))
+
+    
 except KeyboardInterrupt:
-	writeData(process_dict,student_dict, faculty_dict, staff_dict, -1,-1) # Parameters force backup to occur
+	print "Halting Data collection..."
+	sys.exit(0)
+	#writeData(process_dict,student_dict, faculty_dict, staff_dict, -1,-1) # Parameters force backup to occur
 except Exception as e:
+	print "Im outside while"
 	traceback.print_exc()
 	print "Error: {}".format(e)
 	with open("log.error","a") as f:
-		f.write(str(e)+'\n')
-		writeData(process_dict,student_dict, faculty_dict, staff_dict, -1,-1) # Parameters force backup to occur
+		f.write("[{}]: {}\n".format(datetime.datetime.now(),str(e)))
+		#writeData(process_dict,student_dict, faculty_dict, staff_dict, -1,-1) # Parameters force backup to occur
 
